1027324ddc6a92a8ab6a2951f879d55cb36cc37f
juan c <juanchozass@gmail.com>
2023-05-23 10:36:15 +0200

    bottles

---
+++ src/app/bottle/bottle.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottle',
+  templateUrl: './bottle.component.html',
+  styleUrls: ['./bottle.component.scss'],
+})
+export class BottleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
